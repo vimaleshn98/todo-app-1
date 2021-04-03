7d02d50eb40a559aa1cf1e38272c9efa1924939d
vimaleshn98 <vimaleshn98@gmail.com>
2021-04-03 21:10:30 +0530

    changes in jenkinsfile

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 #docker_installation
 
@@ -7,36 +7,36 @@ if [ ! -x /var/lib/docker ]; then
         echo "INSTALLING docker"
 echo
 echo    #install docker dependencies 
-         apt install apt-transport-https ca-certificates curl software-properties-common -y
+         sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 echo
 echo   #add docker’s official GPG key
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo
 echo
         #set up the stable repository.
-	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"	
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"	
 echo
 echo
 	#update the packages
-	apt update -y
+	sudo apt update -y
 
 echo
 echo
 	#checks install from the Docker repo instead of the default Ubuntu repo:
-	apt-cache policy docker-ce 
+	sudo apt-cache policy docker-ce 
 echo
 echo
 
 	#installing dcoker through docker-ce
-	apt install docker-ce -y 
+	sudo apt install docker-ce -y 
 echo
 echo
 	#installing docker-compose
-	apt install docker-compose -y
+	 sudo apt install docker-compose -y
 echo
 echo
        #adds your username to the docker group
-	usermod -aG docker ${USER}
+	sudo usermod -aG docker ${USER}
        
        echo "docker successfully installed"
 
